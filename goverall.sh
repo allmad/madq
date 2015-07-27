4928ed2bb90760b8fea2a9e91cca401a203311a1
Cheney <me@chenye.org>
2015-07-27 13:20:05 +0800

    fix goverall

--- goverall.sh
+++ goverall.sh
@@ -1,23 +1,20 @@
-#!/bin/bash -e
-# usage: ./goverall.sh [func|html]
-# default: generate cover.out
-
 #!/bin/bash
 set -e
 
 # usage: ./goverall.sh [func|html]
 # default: generate cover.out
 output_null=""
+output_file=".cover.out"
 if [[ "$1" != "" ]]; then
 	output_null=">/dev/null"
 fi
 
 mkdir -p .cover
 go list ./... | xargs -I% bash -c 'name="%"; go test % --coverprofile=.cover/${name//\//_} '$output_null
-echo "mode: set" > cover.out
-cat .cover/* | grep -v mode >> cover.out
+echo "mode: set" > $output_file
+cat .cover/* | grep -v mode >> $output_file
 rm -r .cover
 
 if [[ "$1" != "" ]]; then
-	go tool cover -$1=cover.out
+	go tool cover -$1=$output_file
 fi
