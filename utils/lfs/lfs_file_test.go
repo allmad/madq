19f892f36229a7358fb693bfa95022a4b3245107
Cheney <me@chenye.org>
2015-08-17 22:22:35 +0800

    add lfs inode test

--- utils/lfs/lfs_file_test.go
+++ utils/lfs/lfs_file_test.go
@@ -3,11 +3,21 @@ package lfs
 import (
 	"testing"
 
+	"gopkg.in/logex.v1"
+
 	"github.com/chzyer/muxque/utils"
 )
 
 func TestInode(t *testing.T) {
 	var err error
 	defer utils.TDefer(t, &err)
+	InoName := "inode"
+
+	ino := NewInode(InoName, 4)
+	ino.ExtBlks(0, 12, nil)
+	if ino.BlkSize() != 1 {
+		err = logex.NewError("block size not expect")
+		return
+	}
 
 }
