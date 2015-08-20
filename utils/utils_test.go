4f1353a5876be39c96e6c5353a276aa1e9417598
Cheney <me@chenye.org>
2015-08-20 21:34:55 +0800

    fix test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"strings"
 	"testing"
 
 	"gopkg.in/logex.v1"
@@ -10,7 +11,7 @@ func TestGetRoot(t *testing.T) {
 	var err error
 	defer TDefer(t, &err)
 
-	if GetRoot("/hello") != "/data/muxque/hello" {
+	if !strings.HasSuffix(GetRoot("/hello"), "/hello") {
 		err = logex.NewError("result not expected")
 		return
 	}
