235f9efbb4febb156f4b3f652a0c73d1eca45317
chzyer <0@0xdf.com>
2016-06-18 10:34:45 +0800

    [fs] improve file bench

--- go/fs/file_bench_test.go
+++ go/fs/file_bench_test.go
@@ -3,26 +3,31 @@ package fs
 import (
 	"testing"
 
-	"github.com/chzyer/madq/go/bio"
 	"github.com/chzyer/test"
 )
 
-func BenchmarkFile(b *testing.B) {
+func BenchmarkFile1028(b *testing.B) {
+	benchFile(b, 1028)
+}
+
+func nBenchmarkFile102800(b *testing.B) {
+	benchFile(b, 102800)
+}
+
+func benchFile(b *testing.B, size int) {
 	defer test.New(b)
 
-	fd, err := bio.NewFile(test.Root())
-	test.Nil(err)
-	_ = fd
+	// fd, err := bio.NewFile(test.Root())
+	// test.Nil(err)
 	f := testNewFile(test.NewMemDisk())
 	defer f.Close()
 
-	const size = 1028
 	data := test.RandBytes(size)
 
 	for i := 0; i < b.N; i++ {
 		_, err := f.Write(data)
 		test.Nil(err)
-		b.SetBytes(size)
+		b.SetBytes(int64(size))
 	}
 	f.Sync()
 }
