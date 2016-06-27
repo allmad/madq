ef6d60ef0779f537db6937b15b89d2e5eb7b1e37
chzyer <0@0xdf.com>
2016-06-27 23:50:20 +0800

    [bench] fsfile: add stat

--- go/bench/bench_fsfile.go
+++ go/bench/bench_fsfile.go
@@ -4,11 +4,11 @@ import (
 	"crypto/rand"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/chzyer/flow"
-	"github.com/chzyer/madq/go/bio"
-	"github.com/chzyer/madq/go/common"
 	"github.com/chzyer/madq/go/fs"
+	"github.com/chzyer/madq/go/ptrace"
 )
 
 type FsFile struct {
@@ -24,20 +24,16 @@ func (f *FsFile) FlaglyDesc() string {
 func (cfg *FsFile) FlaglyHandle(f *flow.Flow) error {
 	defer f.Close()
 
-	flock, err := common.LockDir(cfg.Dir)
+	now := time.Now()
+
+	vs, err := fs.NewVolumeSource(cfg.Dir)
 	if err != nil {
 		return err
 	}
-	defer flock.Unlock()
-
-	file, err := bio.NewFile(cfg.Dir)
-	if err != nil {
-		return fmt.Errorf("open volume: ", err)
-	}
-	defer file.Close()
+	defer vs.Close()
 
 	vol, err := fs.NewVolume(f, &fs.VolumeConfig{
-		Delegate: bio.NewHybrid(file),
+		Delegate: vs,
 	})
 	if err != nil {
 		return err
@@ -52,12 +48,17 @@ func (cfg *FsFile) FlaglyHandle(f *flow.Flow) error {
 
 	buf := make([]byte, cfg.BlockSize)
 	rand.Read(buf)
+	var size ptrace.Size
+
+	size.AddInt(len(buf) * cfg.BenchCnt)
 
 	for i := 0; i < cfg.BenchCnt; i++ {
 		fd.Write(buf)
 	}
 	fd.Sync()
+	duration := time.Now().Sub(now)
 
 	println(fs.Stat.String())
+	println(size.Rate(duration).String())
 	return nil
 }
