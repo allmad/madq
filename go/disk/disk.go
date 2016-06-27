0071155b782478ff6745fa6d9fa7e72152418ff3
chzyer <0@0xdf.com>
2016-06-27 15:30:09 +0800

    [disk] removed

--- go/disk/disk.go
+++
@@ -1,6 +0,0 @@
-package disk
-
-type Disk interface {
-	ReadAt(b []byte, off int64) (int, error)
-	WriteAt(b []byte, off int64) (int, error)
-}
--- go/disk/memdisk.go
+++
@@ -1,4 +0,0 @@
-package disk
-
-type MemDisk struct {
-}
